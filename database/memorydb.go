f342e331140f4bac45460f49c401116fc0c9989b
kevinpita <69063372+kevinpita@users.noreply.github.com>
2023-07-08 13:15:26 +0200

    Add basic in memory db

---
+++ database/memorydb.go
@@ -0,0 +1,82 @@
+package database
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+type todos map[int]string
+
+type Database struct {
+	mu     sync.Mutex
+	data   todos
+	lastId int
+}
+
+func CreateDatabase() *Database {
+	return &Database{data: make(todos), lastId: -1}
+}
+
+func (db *Database) FetchTodo(id int) (string, error) {
+	var err error
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	val, exists := db.data[id]
+
+	if !exists {
+		err = errors.New(fmt.Sprintf("ID: %v doesn't exist", id))
+	}
+
+	return val, err
+}
+
+func (db *Database) FetchAll() map[int]string {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	todoList := make(map[int]string, len(db.data))
+
+	for k, v := range db.data {
+		todoList[k] = v
+	}
+
+	return todoList
+}
+
+func (db *Database) CreateTodo(todoText string) int {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	db.lastId++
+	db.data[db.lastId] = todoText
+
+	return db.lastId
+}
+
+func (db *Database) UpdateTodo(id int, todoText string) (string, error) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	val, exists := db.data[id]
+
+	if !exists {
+		return "", errors.New(fmt.Sprintf("ID: %v doesn't exist", id))
+	}
+
+	db.data[id] = todoText
+	return val, nil // returns old value as the new one is already known by the caller
+}
+
+func (db *Database) DeleteTodo(id int) (string, error) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	val, exists := db.data[id]
+
+	if !exists {
+		return "", errors.New(fmt.Sprintf("ID: %v doesn't exist", id))
+	}
+
+	return val, nil
+}

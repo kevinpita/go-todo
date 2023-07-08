6459f3dcbfa22b99a2a904e9290e4c14b39acf7b
kevinpita <69063372+kevinpita@users.noreply.github.com>
2023-07-08 21:00:17 +0200

    Log gettodo errors with detailed debug info

--- routes/todo.go
+++ routes/todo.go
@@ -1,9 +1,9 @@
 package routes
 
 import (
-	"errors"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/narvikd/errorskit"
 	"strconv"
 )
 
@@ -15,12 +15,14 @@ func (h handler) GetTodo(c *fiber.Ctx) error {
 	idParam := c.Params("id")
 	id, errConvert := strconv.Atoi(idParam)
 	if errConvert != nil {
-		err := fmt.Errorf("id %v is not from a valid type", idParam)
-		return FailResponse(err.Error(), c, fiber.StatusBadRequest)
+		errorskit.LogWrap(errConvert, "GetTodo invalid parameter")
+		errorString := fmt.Sprintf("id %v is not from a valid type", idParam)
+		return FailResponse(errorString, c, fiber.StatusBadRequest)
 	}
 
 	todoText, err := h.DB.FetchTodo(id)
 	if err != nil {
+		errorskit.LogWrap(err, "GetTodo id not found")
 		return FailResponse(err.Error(), c, fiber.StatusNotFound)
 	}
 
